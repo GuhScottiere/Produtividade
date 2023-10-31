650ce94e4c69a6dd0beca80757c9910f07909da3
Luiz Gustavo Scottiere <gustavo.scottiere@hotmail.com>
2023-10-30 23:27:21 -0300

    comit 1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Produtividade;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      // codigo realizado com base nas instruções do livro produtividade em c#
+      // operadores de atribuição 
+
+      Console.WriteLine(" valores iniciais das variaveis: ");
+      int x = 10;
+      int y = 5;
+      Console.WriteLine($" X: {x} y: {y}");
+      Console.WriteLine();
+
+    }
+}
